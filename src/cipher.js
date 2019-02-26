618bc898bc20061fe9603a14b7bb485b2399c610
Andrealoag <andrea.lopez.agurto@gmail.com>
2019-02-26 13:06:41 -0500

    modifique js

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,17 @@
 window.cipher = {
   encode: () => {
     /* Acá va tu código */
+    function cifrar=(string)=>{
+      let result 
+    }
+
+    }
+    var textcode:""
+
+  
+
+
+
   },
   decode: () => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-/* Acá va tu código */
+/*capturar mensaje
+*/
+const mensaje = document.getElementById("mensaje");
+const boton1 = document.getElementById("boton1");
+const boton2 = document.getElementById("boton2");
+const mensajeMostrado = document.getElementById("mensajeMostrado");
+
+boton1.addEventListener("click",()=>{
+const mensajecapturado = mensaje.value;
+console.log(mensajecapturado);
+mensajeMostrado.innerHTML = mensajecapturado;
+})
+
+boton2.addEventListener("click",()=>{
+    const mensajecapturado = mensaje.value;
+    console.log(mensajecapturado);
+    mensajeMostrado.innerHTML = mensajecapturado;
+})
