9ce5b85f293f8949bd818b98e43c5969091075c6
Andrealoag <andrea.lopez.agurto@gmail.com>
2019-02-27 18:18:01 -0500

    terminando de cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,30 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    function cifrar=(string)=>{
-      let result 
+  encode: (offset,string) => {
+
+    let nuevoMensaje = '';
+    for(let i = 0; i < string.length; i++){
+      if(string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90) {
+        let numberAscci = ((string[i].charCodeAt())-65+offset)%26+65;
+        let lettercipher = String.fromCharCode(numberAscci);
+        nuevoMensaje = nuevoMensaje + lettercipher;
+      } else {
+        nuevoMensaje = nuevoMensaje+string[i];
+      }
     }
+      return nuevoMensaje;
+    },
 
+  decode: (offset,string) => {
+    let nuevoMensaje = '';
+    for(let i = 0; i < string.length; i++){
+      if(string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90) {
+        let numberAscci = ((string[i].charCodeAt())-90-offset)%26+90;
+        let lettercipher = String.fromCharCode(numberAscci);
+        nuevoMensaje = nuevoMensaje + lettercipher;
+      } else {
+        nuevoMensaje = nuevoMensaje+string[i];
+      }
     }
-    var textcode:""
-
-  
-
-
-
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+      return nuevoMensaje;
+    },
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 /*capturar mensaje
 */
-const mensaje = document.getElementById("mensaje");
+const mensaje = document.getElementById("mensaje1");
 const boton1 = document.getElementById("boton1");
 const boton2 = document.getElementById("boton2");
 const mensajeMostrado = document.getElementById("mensajeMostrado");
+const offset = document.getElementById("txtOffset");
 
-boton1.addEventListener("click",()=>{
-const mensajecapturado = mensaje.value;
-console.log(mensajecapturado);
-mensajeMostrado.innerHTML = mensajecapturado;
-})
 
-boton2.addEventListener("click",()=>{
-    const mensajecapturado = mensaje.value;
-    console.log(mensajecapturado);
-    mensajeMostrado.innerHTML = mensajecapturado;
-})
+function cifrar(){
+    let textdeentrada = mensaje.value;
+    let coddespla = parseInt(offset.value);
+    let nuevoMensaje = cipher.encode(coddespla,textdeentrada);
+  mensajeMostrado.value = nuevoMensaje;
+}
+boton1.addEventListener("click",cifrar);
+
+function descifrar(){
+    let textdeentrada = mensaje.value;
+    let coddespla = parseInt(offset.value);
+    let nuevoMensaje = cipher.decode(coddespla,textdeentrada);
+  mensajeMostrado.value = nuevoMensaje;
+}
+boton2.addEventListener("click",descifrar);
+    
